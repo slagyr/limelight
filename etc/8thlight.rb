e48e8de38d63c8c476655cd6176df4e9edbd7e3e
micah <micah@1c3bf517-0e11-eda0-a599-a21a5701d406>
2007-10-11 13:13:42 +0000

    fixing widths and heights
    git-svn-id: http://svn.8thlight.com/limelight/trunk@2 1c3bf517-0e11-eda0-a599-a21a5701d406

--- etc/8thlight.rb
+++ etc/8thlight.rb
@@ -48,25 +48,25 @@ We at 8th Light believe craftsmanship and quality are the path to success.
     
 We specialize in writing high quality custom applications.
 END
-    page = Page.new(:width => 1000, :height => 900, :horizontal_alignment => :center, :background_image => "#{IMAGES}/bg.jpg")
-    main_column = page << Block.new(:border_width => 0, :background_image => "#{IMAGES}/canvas_bg.jpg", :width => 823, :height => 850, :horizontal_alignment => :center)
-    header = main_column << Block.new(:height => 200, :background_image => "#{IMAGES}/header.jpg", :width => 823, :horizontal_alignment => :center)
-    menu = main_column << Block.new(:height => 67, :top_border_width => 1, :bottom_border_width => 1, :border_color => "#333333", :width => 823, :horizontal_alignment => :right, :right_margin => 25, :left_margin => 25, :bottom_margin => 15)
+    page = Page.new(:width => "1000", :height => "900", :horizontal_alignment => :center, :background_image => "#{IMAGES}/bg.jpg")
+    main_column = page << Block.new(:border_width => 0, :background_image => "#{IMAGES}/canvas_bg.jpg", :width => "823", :height => "850", :horizontal_alignment => :center)
+    header = main_column << Block.new(:height => "200", :background_image => "#{IMAGES}/header.jpg", :width => "823", :horizontal_alignment => :center)
+    menu = main_column << Block.new(:height => "67", :top_border_width => 1, :bottom_border_width => 1, :border_color => "#333333", :width => "823", :horizontal_alignment => :right, :right_margin => 25, :left_margin => 25, :bottom_margin => 15)
      ["Home", "Services", "About", "Blog", "Contact"].each do |text|
-       link = menu << Block.new(:text => text, :width => 80, :height => 50, :text_color => "#0049F4", :font_face => "Arial", :font_size => 13, :vertical_alignment => :middle, :horizontal_alignment => :center)
+       link = menu << Block.new(:text => text, :width => "80", :height => "50", :text_color => "#0049F4", :font_face => "Arial", :font_size => 13, :vertical_alignment => :middle, :horizontal_alignment => :center)
        # link.on_mouse_enter { link.style :color => "#ffffff", :background_image => "#{IMAGES}/button_bg.jpg" }
        # link.on_mouse_leave { link.style :color => "#0049F4", :background_image => "#{IMAGES}/button_bg_ie.jpg" }
      end
-    tag_line = main_column << Block.new(:width => 823, :height => 200, :background_image => "#{IMAGES}/tag_line.jpg", :right_margin => 25, :left_margin => 25, :horizontal_alignment => :left )
-    tag_line_content = tag_line << Block.new(:text => tag_line_text, :width => 400, :height => 150, :top_margin => 50, :left_margin => 50, :text_color => :white, :font_size => 14)
+    tag_line = main_column << Block.new(:width => "823", :height => "200", :background_image => "#{IMAGES}/tag_line.jpg", :right_margin => 25, :left_margin => 25, :horizontal_alignment => :left )
+    tag_line_content = tag_line << Block.new(:text => tag_line_text, :width => "400", :height => "150", :top_margin => 50, :left_margin => 50, :text_color => :white, :font_size => 14)
     
-    spot_light = main_column << Block.new(:width => 412, :height => 300, :top_margin => 20, :left_margin => 50, :right_margin => 50)
-    spot_light_title = spot_light << Block.new(:text => "In the spotlight", :width => 312, :height => 35, :bottom_border_width => 1, :border_color => "#0049F4", :font_size => 20, :text_color => "white")
-    news = main_column << Block.new(:width => 411, :height => 300, :top_margin => 20, :left_margin => 50, :right_margin => 50)
-    spot_light_title = news << Block.new(:text => "Recent News", :width => 311, :height => 35, :bottom_border_width => 1, :border_color => "#0049F4", :font_size => 20, :text_color => "white")
+    spot_light = main_column << Block.new(:width => "412", :height => "300", :top_margin => 20, :left_margin => 50, :right_margin => 50)
+    spot_light_title = spot_light << Block.new(:text => "In the spotlight", :width => "312", :height => "35", :bottom_border_width => 1, :border_color => "#0049F4", :font_size => 20, :text_color => "white")
+    news = main_column << Block.new(:width => "411", :height => "300", :top_margin => 20, :left_margin => 50, :right_margin => 50)
+    spot_light_title = news << Block.new(:text => "Recent News", :width => "311", :height => "35", :bottom_border_width => 1, :border_color => "#0049F4", :font_size => 20, :text_color => "white")
     
-    footer = main_column << Block.new(:background_image => "#{IMAGES}/footer_bg.jpg", :height => 80, :width => 823)
-    copyright = footer << Block.new(:text => "8th Light, Inc. Copyright 2007", :top_margin => 40, :vertical_alignment => :middle, :horizontal_alignment => :center, :text_color => "#666", :width => 823, :height => 80)
+    footer = main_column << Block.new(:background_image => "#{IMAGES}/footer_bg.jpg", :height => "80", :width => "823")
+    copyright = footer << Block.new(:text => "8th Light, Inc. Copyright 2007", :top_margin => 40, :vertical_alignment => :middle, :horizontal_alignment => :center, :text_color => "#666", :width => "823", :height => "80")
 
     return page
   end
