81fe903fb652288607a55256d01209a2841a3cef
micah <micah@1c3bf517-0e11-eda0-a599-a21a5701d406>
2008-02-21 23:32:22 +0000

    renamed all instances of controller to players in examples
    git-svn-id: http://svn.8thlight.com/limelight/trunk@48 1c3bf517-0e11-eda0-a599-a21a5701d406

--- ruby_src/lib/limelight/llm_parser.rb
+++ ruby_src/lib/limelight/llm_parser.rb
@@ -2,7 +2,7 @@ require "rexml/document"
 require 'limelight/page'
 require 'limelight/block'
 require 'limelight/styles'
-require 'limelight/players/controllers'
+require 'limelight/players/players'
 
 module Limelight
   class LlmParser
