e06124beb4c363223980d90d1bd154aa8ea06b55
micah <micah@1c3bf517-0e11-eda0-a599-a21a5701d406>
2008-02-28 17:57:38 +0000

    added header to examples
    git-svn-id: http://svn.8thlight.com/limelight/trunk@54 1c3bf517-0e11-eda0-a599-a21a5701d406

---
+++ examples/sandbox/header.rb
@@ -0,0 +1,8 @@
+header do
+  example_link :text => "Click Me", :on_mouse_clicked => "book.load('click_me')"
+  example_link :text => "Teaser (color transparency)", :on_mouse_clicked => "book.load('teaser')"
+  example_link :text => "Homer (transparency)", :on_mouse_clicked => "book.load('homer')"
+  example_link :text => "Gradients", :on_mouse_clicked => "book.load('gradients')"
+  example_link :text => "Scrolling", :on_mouse_clicked => "book.load('scrolling')"
+  example_link :text => "Inputs", :on_mouse_clicked => "book.load('inputs')"
+end
