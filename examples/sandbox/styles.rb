76b2540fbb407d5f2cef8a9ed19cc685a5848e82
micah <micah@1c3bf517-0e11-eda0-a599-a21a5701d406>
2008-02-28 17:46:20 +0000

    refactored all the examples to use the new directory structure
    git-svn-id: http://svn.8thlight.com/limelight/trunk@53 1c3bf517-0e11-eda0-a599-a21a5701d406

--- examples/sandbox/styles.rb
+++ examples/sandbox/styles.rb
@@ -4,3 +4,19 @@ sandbox {
 	horizontal_alignment :center
 	vertical_alignment :center
 }
+
+header {
+  width 900
+  height 50
+  background_color :grey
+  border_color :blue
+  border_width 1
+}
+
+example_link {
+  font_size 8
+  text_color :blue
+  hover {
+    text_color :white
+  }
+}
