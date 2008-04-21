1230ffcb10a9fc111683f43983da7ca1591df04c
micah <micah@1c3bf517-0e11-eda0-a599-a21a5701d406>
2008-04-21 21:08:30 +0000

    preparing to make installers
    git-svn-id: http://svn.8thlight.com/limelight/trunk@87 1c3bf517-0e11-eda0-a599-a21a5701d406

--- bin/limelight.rb
+++ bin/limelight.rb
@@ -1,5 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + "/../ruby_src/lib/init")
 require 'limelight/producer'
 
+puts "ARGV"
+10.times do |i|
+  puts "#{i}: #{ARGV[i]}"
+end
+
+puts "$LIMELIGHT_STARTUP_PRODUCTION: #{$LIMELIGHT_STARTUP_PRODUCTION}"
+
 page_name = ARGV[0]
 Limelight::Producer.open(page_name)
--- ruby_src/lib/init.rb
+++ ruby_src/lib/init.rb
@@ -1,3 +1,13 @@
+# $LIMELIGHT_LIB = File.expand_path(File.dirname(__FILE__))
+# $LIMELIGHT_HOME = File.expand_path(File.join($LIMELIGHT_LIB, "..", ".."))
+# $RUBY_LIB = File.join($LIMELIGHT_HOME, "jars")
+# 
+# $: << $RUBY_LIB
+# $: << File.join($RUBY_LIB, "jars", "ruby", "site_ruby", "1.8")
+# $: << File.join($RUBY_LIB, "jars", "ruby", "site_ruby", "1.8", "java")
+# $: << File.join($RUBY_LIB, "jars", "ruby", "site_ruby")
+# $: << File.join($RUBY_LIB, "jars", "ruby", "1.8")
+# $: << File.join($RUBY_LIB, "jars", "ruby", "1.8", "java")
 $: << File.expand_path(File.dirname(__FILE__))
 
 require 'java'
