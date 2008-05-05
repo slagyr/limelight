3d661777e761bcdf44f4019d224defd635fb385c
micah <micah@1c3bf517-0e11-eda0-a599-a21a5701d406>
2008-05-05 18:59:13 +0000

    added RishStyle class... not integrated
    git-svn-id: http://svn.8thlight.com/limelight/trunk@104 1c3bf517-0e11-eda0-a599-a21a5701d406

---
+++ src/limelight/ui/RichStyle.java
@@ -0,0 +1,151 @@
+package limelight.ui;
+
+import limelight.Util;
+
+import java.util.LinkedList;
+
+public class RichStyle extends Style
+{
+  private String[] styles;
+  private LinkedList<RichStyle> extenders;
+  private LinkedList<RichStyle> extensions;
+
+  public RichStyle()
+  {
+    styles = new String[STYLE_COUNT];
+  }
+
+  public String get(int key)
+  {
+    if(styles[key] != null)
+      return styles[key];
+    else if(extensions != null)
+    {
+      return getFrom(extensions, key);
+    }
+
+    return null;
+  }
+
+  public void put(StyleDescriptor descriptor, String value)
+  {
+    if(value == null)
+      return;
+    value = value.trim();
+    if(value.length() == 0)
+      value = null;
+
+    String originalValue = styles[descriptor.index];
+    styles[descriptor.index] = value;
+    if(!Util.equal(originalValue, value))
+    {
+      changes[descriptor.index] = true;
+      notifyExtendersOfChange(descriptor, value);
+    }
+  }
+
+  protected boolean has(int key)
+  {
+    return styles[key] != null;
+  }
+
+  public void removeExtension(RichStyle extension)
+  {
+    if(extensions != null)
+    {
+      extension.removeExtender(this);
+      applyChangesFromExtension(extension);
+      extensions.remove(extension);
+    }
+  }
+
+  private void removeExtender(RichStyle extender)
+  {
+    if(extenders != null)
+      extenders.remove(extender);
+  }
+
+  public void addExtension(RichStyle extension)
+  {
+    if(extensions == null)
+      extensions = new LinkedList<RichStyle>();
+
+    if(extension != null && !hasExtension(extension))
+    {
+      applyChangesFromExtension(extension);
+      extensions.add(extension);
+      if(!extension.hasExtender(this))
+        extension.addExtender(this);
+    }
+  }
+
+  private void addExtender(RichStyle extender)
+  {
+    if(extenders == null)
+      extenders = new LinkedList<RichStyle>();
+    extenders.add(extender);
+  }
+
+  private void notifyExtendersOfChange(StyleDescriptor descriptor, String value)
+  {
+    if(extenders != null)
+    {
+      for(RichStyle extender : extenders)
+        extender.styleChanged(descriptor, value);
+    }
+  }
+
+  private void styleChanged(StyleDescriptor descriptor, String value)
+  {
+    if(styles[descriptor.index] == null)
+    {
+      changes[descriptor.index] = true;
+      notifyExtendersOfChange(descriptor, value);
+    }
+  }
+
+  private void applyChangesFromExtension(RichStyle style)
+  {
+    LinkedList<RichStyle> seniorExtensions = findSeniorExtensions(style);
+
+    for(StyleDescriptor descriptor : Style.STYLE_LIST)
+    {
+      String value = style.get(descriptor.index);
+      if(value != null && getFrom(seniorExtensions, descriptor.index) == null)
+        styleChanged(descriptor, value);
+    }
+  }
+
+  private LinkedList<RichStyle> findSeniorExtensions(RichStyle style)
+  {
+    LinkedList<RichStyle> seniorExtensions = new LinkedList<RichStyle>();
+    for(RichStyle extension : extensions)
+    {
+      if(extension == style)
+        break;
+      seniorExtensions.add(extension);
+    }
+    return seniorExtensions;
+  }
+
+  private String getFrom(LinkedList<RichStyle> extensions, int key)
+  {
+    for(Style style : extensions)
+    {
+      String value = style.get(key);
+      if(value != null)
+        return value;
+    }
+    return null;
+  }
+
+  public boolean hasExtender(RichStyle style)
+  {
+    return extenders != null && extenders.contains(style);
+  }
+
+  public boolean hasExtension(RichStyle style)
+  {
+    return extensions != null && extensions.contains(style);
+  }
+}
---
+++ src/limelight/ui/RichStyleTest.java
@@ -0,0 +1,174 @@
+package limelight.ui;
+
+import junit.framework.TestCase;
+import limelight.ui.RichStyle;
+import limelight.ui.Style;
+import limelight.ui.StyleDescriptor;
+
+public class RichStyleTest extends TestCase
+{
+  private RichStyle style;
+  private RichStyle style2;
+  private RichStyle style3;
+  private RichStyle style4;
+  private RichStyle newStyle;
+  private RichStyle anotherNewStyle;
+
+  public void setUp() throws Exception
+  {
+    style = new RichStyle();
+    style2 = new RichStyle();
+    style3 = new RichStyle();
+    style4 = new RichStyle();
+    newStyle = new RichStyle();
+    anotherNewStyle = new RichStyle();
+  }
+
+  public void testHasDefaultsFirst() throws Exception
+  {
+    for(StyleDescriptor descriptor : Style.STYLE_LIST)
+      assertEquals(descriptor.defaultValue, style.get(descriptor));
+  }
+  
+  public void testCanSetNewValues() throws Exception
+  {
+    style.setBackgroundColor("blue");
+    assertEquals("blue", style.getBackgroundColor());
+    assertEquals("blue", style.get(Style.BACKGROUND_COLOR));
+  }
+
+  public void testInsetNext() throws Exception
+  {
+    style.addExtension(style2);
+    assertEquals(true, style.hasExtension(style2));
+    assertEquals(true, style2.hasExtender(style));
+
+    style.addExtension(style3);
+    assertEquals(true, style.hasExtension(style3));
+    assertEquals(true, style3.hasExtender(style));
+  }
+
+  public void testRemoveExtension() throws Exception
+  {
+    style.addExtension(style2);
+    style2.addExtension(style3);
+
+    style.removeExtension(style2);
+
+    assertEquals(true, style2.hasExtension(style3));
+    assertEquals(false, style.hasExtension(style2));
+  }
+
+  public void testWithNothingStacked() throws Exception
+  {
+    style.setWidth("100");
+    assertEquals("100", style.getWidth());
+    assertEquals(true, style.changed());
+    assertEquals(true, style.changed(Style.WIDTH));
+  }
+
+  public void testChangesFromBelow() throws Exception
+  {
+    style.addExtension(style2);
+    style.addExtension(style3);
+
+    assertFalse(style.changed());
+
+    style.setWidth("123");
+    assertTrue(style.changed());
+
+    style.flushChanges();
+    assertFalse(style.changed());
+
+    style2.setWidth("123");
+    assertFalse(style.changed());
+
+    style.flushChanges();
+    assertFalse(style.changed());
+
+    style3.setWidth("123");
+    assertFalse(style.changed());
+
+    style3.setHeight("321");
+    assertTrue(style.changed());
+
+    style.flushChanges();
+    assertFalse(style.changed());
+  }
+
+  public void testSpecificChanges() throws Exception
+  {
+    style.addExtension(style2);
+    style.addExtension(style3);
+    
+    assertFalse(style.changed(Style.WIDTH));
+    assertFalse(style.changed(Style.HEIGHT));
+
+    style3.setWidth("123");
+    assertTrue(style.changed(Style.WIDTH));
+    assertFalse(style.changed(Style.HEIGHT));
+    style.flushChanges();
+    assertFalse(style.changed(Style.WIDTH));
+    assertFalse(style.changed(Style.HEIGHT));
+
+    style2.setWidth("123");
+    assertTrue(style.changed(Style.WIDTH));
+    assertFalse(style.changed(Style.HEIGHT));
+    style.flushChanges();
+    assertFalse(style.changed(Style.WIDTH));
+    assertFalse(style.changed(Style.HEIGHT));
+  }
+
+  public void testAddingToBottomAffectsChanges() throws Exception
+  {
+    style.setWidth("100");
+    style.flushChanges();
+
+    newStyle.setWidth("200");
+
+    style.addExtension(newStyle);
+    assertFalse(style.changed());
+
+    anotherNewStyle.setHeight("100");
+    style.addExtension(anotherNewStyle);
+    assertTrue(style.changed());
+    assertTrue(style.changed(Style.HEIGHT));
+  }
+
+  public void testRemoveFromBottomAffectsChanges() throws Exception
+  {
+    newStyle.setWidth("100");
+    anotherNewStyle.setWidth("200");
+
+    style.addExtension(newStyle);
+    style.addExtension(anotherNewStyle);
+    style.flushChanges();
+
+    style.removeExtension(anotherNewStyle);
+    assertFalse(style.changed());
+
+    style.removeExtension(newStyle);
+    assertTrue(style.changed());
+    assertTrue(style.changed(Style.WIDTH));
+  }
+
+  public void testCanHaveLotsOfExtenders() throws Exception
+  {
+    style.setWidth("100");
+
+    style2.addExtension(style);
+    style3.addExtension(style);
+    style4.addExtension(style);
+
+    assertEquals(true, style2.hasExtension(style));
+    assertEquals(true, style3.hasExtension(style));
+    assertEquals(true, style4.hasExtension(style));
+    assertEquals(true, style.hasExtender(style2)); 
+    assertEquals(true, style.hasExtender(style3));
+    assertEquals(true, style.hasExtender(style4));
+    assertEquals("100", style2.getWidth());
+    assertEquals("100", style3.getWidth());
+    assertEquals("100", style4.getWidth());
+  }
+
+}
