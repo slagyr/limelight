e778a3064ed777950ad9b813c868c89073482c2d
micah <micah@1c3bf517-0e11-eda0-a599-a21a5701d406>
2008-05-12 03:30:47 +0000

    modified the limelight command to be extensile.  Added pack command to create .llp files
    git-svn-id: http://svn.8thlight.com/limelight/trunk@113 1c3bf517-0e11-eda0-a599-a21a5701d406

---
+++ lib/limelight/commands.rb
@@ -0,0 +1,63 @@
+require 'optparse'
+
+module Limelight
+
+  DEFAULT_PRODUCTION = File.expand_path(File.dirname(__FILE__) + "/../../productions/startup")
+
+  module Commands
+
+    class OpenProduction
+      def self.description
+        return "Open a limelight production or scene."
+      end
+
+      def run(args)
+        Java::limelight::Main.initialize_context
+        require 'limelight/producer'
+        production = args.length > 0 ? args[0] : DEFAULT_PRODUCTION
+        Limelight::Producer.open(production)
+      end
+    end
+
+    class PackProduction
+      def self.description
+        return "Pack the designated limelight production into a .llp file."
+      end
+
+      def run(args)
+        packer = Java::limelight::io::Packer.new
+        packer.pack(args.shift)
+      end
+    end
+
+    COMMANDS = {
+        "open" => OpenProduction,
+        "pack" => PackProduction
+    }
+
+    class << self
+      def run(args)
+        command_name = args.shift
+        command = COMMANDS[command_name]
+        if command
+          command.new.run(args)
+        else
+          usage
+        end
+      end
+
+      def usage
+        puts "Usage: limelight <command> [options] [params]"
+        puts "commands:"
+        COMMANDS.keys.sort.each do |key|
+          command = COMMANDS[key]
+          puts "\t#{key}\t\t#{command.description}"
+        end
+      end
+    end
+
+
+  end
+
+
+end
--- src/limelight/Main.java
+++ src/limelight/Main.java
@@ -1,20 +1,18 @@
 package limelight;
 
+import limelight.io.FileUtil;
 import limelight.io.TempDirectory;
 import org.jruby.Ruby;
 import org.jruby.RubyInstanceConfig;
-import org.jruby.RubyString;
-import org.jruby.runtime.GlobalVariable;
-
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 public class Main
 {
   public static String LIMELIGHT_HOME = System.getProperty("limelight.home");
   private RubyInstanceConfig config;
   private Ruby runtime;
+  private boolean contextIsConfigured;
 
   public static void main(String[] args) throws Exception
   {
@@ -31,22 +29,19 @@ public class Main
     configureSystemProperties();
     processArgs(args);
     runtime = Ruby.newInstance(config);
-    injectStartupProduction(getStartupProduction());
     configureContext();
-    startJrubyRuntime();
+    startJrubyRuntime(getStartupProduction());
   }
 
-  private void startJrubyRuntime() throws FileNotFoundException
+  private void startJrubyRuntime(String startupProduction) throws FileNotFoundException
   {
-    String mainRubyFile = LIMELIGHT_HOME + "/lib/limelight.rb";
-    InputStream in = new FileInputStream(mainRubyFile);
-    runtime.runFromMain(in, mainRubyFile);
-  }
+    StringBuffer startupRuby = new StringBuffer();
+    startupRuby.append("require '").append(FileUtil.pathTo(LIMELIGHT_HOME, "lib", "init")).append("'").append("\n");
+    startupRuby.append("require 'limelight/producer'").append("\n");
+    startupRuby.append("Limelight::Producer.open('").append(startupProduction).append("')").append("\n");
 
-  private void injectStartupProduction(String productionName)
-  {
-    runtime.defineGlobalConstant("LIMELIGHT_STARTUP_PRODUCTION", RubyString.newString(runtime, productionName));
-    runtime.defineVariable(new GlobalVariable(runtime, "LIMELIGHT_STARTUP_PRODUCTION", RubyString.newString(runtime, "examples/sandbox")));
+    ByteArrayInputStream input = new ByteArrayInputStream(startupRuby.toString().getBytes());
+    runtime.runFromMain(input, "limelight_pseudo_main");
   }
 
   private String getStartupProduction()
@@ -82,7 +77,10 @@ public class Main
 
   public void configureContext()
   {
+    if(contextIsConfigured)
+      return;
     Context context = Context.instance();
     context.tempDirectory = new TempDirectory();
+    contextIsConfigured = true;
   }
 }
