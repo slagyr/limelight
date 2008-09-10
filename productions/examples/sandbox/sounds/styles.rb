f799c8fea29712c940603b8ec98388f1261f22bf
Micah Martin <micah@8thlight.com>
2008-09-09 21:49:00 -0500

    updated the copyright

--- productions/examples/sandbox/sounds/styles.rb
+++ productions/examples/sandbox/sounds/styles.rb
@@ -1,3 +1,6 @@
+#- Copyright 2008 8th Light, Inc. All Rights Reserved.
+#- Limelight and all included source files are distributed under terms of the GNU LGPL.
+
 clip {
   width "100%"
   font_size 20
