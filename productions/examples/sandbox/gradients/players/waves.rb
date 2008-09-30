70b2d171ad6c2a61ca149046ac360979c2eb1ce3
Micah Martin <micah@8thlight.com>
2008-09-30 10:34:07 -0500

    fixed gradients in sample

--- productions/examples/sandbox/gradients/players/waves.rb
+++ productions/examples/sandbox/gradients/players/waves.rb
@@ -6,8 +6,8 @@ module Waves
   def mouse_entered(e)
     @step = -1
     @animation = animate(:updates_per_second => 30) do
-      @step = -1 if style.gradient_penetration == "100"
-      @step = 1 if style.gradient_penetration == "1"
+      @step = -1 if style.gradient_penetration == "100%"
+      @step = 1 if style.gradient_penetration == "1%"
       style.gradient_penetration = (style.gradient_penetration.to_i + @step).to_s
     end
   end
