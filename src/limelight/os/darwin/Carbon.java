1febcdf54aa430669ecfb9eca46175d8dbaf1b86
Micah Martin <micah@8thlight.com>
2009-05-06 21:13:04 -0500

    using JNA instead of JNI

---
+++ src/limelight/os/darwin/Carbon.java
@@ -0,0 +1,37 @@
+package limelight.os.darwin;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.NativeLibrary;
+import com.sun.jna.ptr.IntByReference;
+
+public interface Carbon extends Library
+{
+  Carbon INSTANCE = (Carbon) Native.loadLibrary("Carbon", Carbon.class);
+  NativeLibrary library = NativeLibrary.getInstance("Carbon");
+
+  //SystemUIMode
+  int kUIModeNormal = 0;
+  int kUIModeContentSuppressed = 1;
+  int kUIModeContentHidden = 2;
+  int kUIModeAllSuppressed = 4;
+  int kUIModeAllHidden = 3;
+
+  //SystemUIOptions
+  int kUIOptionAutoShowMenuBar = 1 << 0;
+  int kUIOptionDisableAppleMenu = 1 << 2;
+  int kUIOptionDisableProcessSwitch = 1 << 3;
+  int kUIOptionDisableForceQuit = 1 << 4;
+  int kUIOptionDisableSessionTerminate = 1 << 5;
+  int kUIOptionDisableHide = 1 << 6;
+
+//  SystemUIMode originalMode;
+//SystemUIOptions originalOptions;
+
+//  public int GetSystemUIMode(Pointer mode, Pointer options);
+  public int GetSystemUIMode(IntByReference mode, IntByReference options);
+
+  public int SetSystemUIMode(int mode, int options);
+
+}
---
+++ src/limelight/os/darwin/DarwinOSTrial.java
@@ -0,0 +1,43 @@
+package limelight.os.darwin;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.*;
+
+public class DarwinOSTrial
+{
+  public static void main(String[] args)
+  {
+    final DarwinOS os = new DarwinOS();
+    os.enterKioskMode();
+
+    JFrame frame = new JFrame();
+    frame.setSize(400, 400);
+
+    final JButton button = new JButton("Exit Kiosk Mode");
+    button.addActionListener(new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        if(os.isInKioskMode())
+        {
+          os.exitKioskMode();
+          button.setText("Enter Kiosk Mode");
+        }
+        else
+        {
+          os.enterKioskMode();
+          button.setText("Exit Kiosk Mode");
+        }
+      }
+    });
+    frame.add(button);
+
+
+    frame.setVisible(true);
+    frame.setAlwaysOnTop(true);
+    GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().setFullScreenWindow(frame);
+  }
+
+}
