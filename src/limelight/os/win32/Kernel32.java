6d8128c25f96c3cb857cd06753e432b9c0a319dc
Micah Martin <micah@8thlight.com>
2009-05-07 11:59:44 -0500

    added native kiosk code for win32

---
+++ src/limelight/os/win32/Kernel32.java
@@ -0,0 +1,273 @@
+package limelight.os.win32;
+
+import com.sun.jna.Native;
+import com.sun.jna.Structure;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.PointerByReference;
+import com.sun.jna.ptr.IntByReference;
+
+import java.nio.Buffer;
+
+public interface Kernel32 extends W32API
+{
+
+  Kernel32 INSTANCE = (Kernel32) Native.loadLibrary("kernel32", Kernel32.class, DEFAULT_OPTIONS);
+
+  class SYSTEMTIME extends Structure
+  {
+    public short wYear;
+    public short wMonth;
+    public short wDayOfWeek;
+    public short wDay;
+    public short wHour;
+    public short wMinute;
+    public short wSecond;
+    public short wMilliseconds;
+  }
+
+  Pointer LocalFree(Pointer hLocal);
+
+  Pointer GlobalFree(Pointer hGlobal);
+
+  HMODULE GetModuleHandle(String name);
+
+  void GetSystemTime(SYSTEMTIME result);
+
+  int GetCurrentThreadId();
+
+  HANDLE GetCurrentThread();
+
+  int GetCurrentProcessId();
+
+  HANDLE GetCurrentProcess();
+
+  int GetProcessId(HANDLE process);
+
+  int GetProcessVersion(int processId);
+
+  int GetLastError();
+
+  void SetLastError(int dwErrCode);
+
+  int GetDriveType(String rootPathName);
+
+  int FORMAT_MESSAGE_ALLOCATE_BUFFER = 0x0100;
+  int FORMAT_MESSAGE_FROM_SYSTEM = 0x1000;
+  int FORMAT_MESSAGE_IGNORE_INSERTS = 0x200;
+
+  int FormatMessage(int dwFlags, Pointer lpSource, int dwMessageId,
+                    int dwLanguageId, PointerByReference lpBuffer,
+                    int nSize, Pointer va_list);
+
+  int FormatMessage(int dwFlags, Pointer lpSource, int dwMessageId,
+                    int dwLanguageId, Buffer lpBuffer,
+                    int nSize, Pointer va_list);
+
+  int FILE_LIST_DIRECTORY = 0x00000001;
+
+  int FILE_SHARE_READ = 1;
+  int FILE_SHARE_WRITE = 2;
+  int FILE_SHARE_DELETE = 4;
+
+  int CREATE_NEW = 1;
+  int CREATE_ALWAYS = 2;
+  int OPEN_EXISTING = 3;
+  int OPEN_ALWAYS = 4;
+  int TRUNCATE_EXISTING = 5;
+
+  int FILE_FLAG_WRITE_THROUGH = 0x80000000;
+  int FILE_FLAG_OVERLAPPED = 0x40000000;
+  int FILE_FLAG_NO_BUFFERING = 0x20000000;
+  int FILE_FLAG_RANDOM_ACCESS = 0x10000000;
+  int FILE_FLAG_SEQUENTIAL_SCAN = 0x08000000;
+  int FILE_FLAG_DELETE_ON_CLOSE = 0x04000000;
+  int FILE_FLAG_BACKUP_SEMANTICS = 0x02000000;
+  int FILE_FLAG_POSIX_SEMANTICS = 0x01000000;
+  int FILE_FLAG_OPEN_REPARSE_POINT = 0x00200000;
+  int FILE_FLAG_OPEN_NO_RECALL = 0x00100000;
+
+  int FILE_ATTRIBUTE_READONLY = 0x00000001;
+  int FILE_ATTRIBUTE_HIDDEN = 0x00000002;
+  int FILE_ATTRIBUTE_SYSTEM = 0x00000004;
+  int FILE_ATTRIBUTE_DIRECTORY = 0x00000010;
+  int FILE_ATTRIBUTE_ARCHIVE = 0x00000020;
+  int FILE_ATTRIBUTE_DEVICE = 0x00000040;
+  int FILE_ATTRIBUTE_NORMAL = 0x00000080;
+  int FILE_ATTRIBUTE_TEMPORARY = 0x00000100;
+  int FILE_ATTRIBUTE_SPARSE_FILE = 0x00000200;
+  int FILE_ATTRIBUTE_REPARSE_POINT = 0x00000400;
+  int FILE_ATTRIBUTE_COMPRESSED = 0x00000800;
+  int FILE_ATTRIBUTE_OFFLINE = 0x00001000;
+  int FILE_ATTRIBUTE_NOT_CONTENT_INDEXED = 0x00002000;
+  int FILE_ATTRIBUTE_ENCRYPTED = 0x00004000;
+
+  int DRIVE_UNKNOWN = 0;
+  int DRIVE_NO_ROOT_DIR = 1;
+  int DRIVE_REMOVABLE = 2;
+  int DRIVE_FIXED = 3;
+  int DRIVE_REMOTE = 4;
+  int DRIVE_CDROM = 5;
+  int DRIVE_RAMDISK = 6;
+
+  int GENERIC_WRITE = 0x40000000;
+
+  class SECURITY_ATTRIBUTES extends Structure
+  {
+    public int nLength = size();
+    public Pointer lpSecurityDescriptor;
+    public boolean bInheritHandle;
+  }
+
+  HANDLE CreateFile(String lpFileName, int dwDesiredAccess, int dwShareMode,
+                    SECURITY_ATTRIBUTES lpSecurityAttributes,
+                    int dwCreationDisposition, int dwFlagsAndAttributes,
+                    HANDLE hTemplateFile);
+
+  boolean CreateDirectory();
+
+  HANDLE CreateIoCompletionPort(HANDLE FileHandle,
+                                HANDLE ExistingCompletionPort,
+                                Pointer CompletionKey,
+                                int NumberOfConcurrentThreads);
+
+  int INFINITE = 0xFFFFFFFF;
+
+  boolean GetQueuedCompletionStatus(HANDLE CompletionPort,
+                                    IntByReference lpNumberOfBytes,
+                                    Structure.ByReference lpCompletionKey,
+                                    PointerByReference lpOverlapped,
+                                    int dwMilliseconds);
+
+  boolean PostQueuedCompletionStatus(HANDLE CompletionPort,
+                                     int dwNumberOfBytesTransferred,
+                                     Pointer dwCompletionKey,
+                                     OVERLAPPED lpOverlapped);
+
+  int WaitForSingleObject(HANDLE hHandle, int dwMilliseconds);
+
+  boolean DuplicateHandle(HANDLE hSourceProcessHandle,
+                          HANDLE hSourceHandle,
+                          HANDLE hTargetProcessHandle,
+                          HANDLEByReference lpTargetHandle,
+                          int dwDesiredAccess,
+                          boolean bInheritHandle,
+                          int dwOptions);
+
+  boolean CloseHandle(HANDLE hObject);
+
+  int FILE_ACTION_ADDED = 1;
+  int FILE_ACTION_REMOVED = 2;
+  int FILE_ACTION_MODIFIED = 3;
+  int FILE_ACTION_RENAMED_OLD_NAME = 4;
+  int FILE_ACTION_RENAMED_NEW_NAME = 5;
+
+  int FILE_NOTIFY_CHANGE_FILE_NAME = 1;
+  int FILE_NOTIFY_CHANGE_DIR_NAME = 2;
+  int FILE_NOTIFY_CHANGE_NAME = 3;
+  int FILE_NOTIFY_CHANGE_ATTRIBUTES = 4;
+  int FILE_NOTIFY_CHANGE_SIZE = 8;
+  int FILE_NOTIFY_CHANGE_LAST_WRITE = 16;
+  int FILE_NOTIFY_CHANGE_LAST_ACCESS = 32;
+  int FILE_NOTIFY_CHANGE_CREATION = 64;
+  int FILE_NOTIFY_CHANGE_EA = 128;
+  int FILE_NOTIFY_CHANGE_SECURITY = 256;
+  int FILE_NOTIFY_CHANGE_STREAM_NAME = 512;
+  int FILE_NOTIFY_CHANGE_STREAM_SIZE = 1024;
+  int FILE_NOTIFY_CHANGE_STREAM_WRITE = 2048;
+
+  /**
+   * This structure is non-trivial since it is a pattern stamped
+   * into a large block of result memory rather than something that stands
+   * alone or is used for input.
+   */
+  class FILE_NOTIFY_INFORMATION extends Structure
+  {
+    public int NextEntryOffset;
+    public int Action;
+    public int FileNameLength;
+    // filename is not nul-terminated, so we can't use a String/WString
+    public char[] FileName = new char[1];
+
+    private FILE_NOTIFY_INFORMATION()
+    {
+    }
+
+    public FILE_NOTIFY_INFORMATION(int size)
+    {
+      if(size < size())
+        throw new IllegalArgumentException("Size must greater than "
+            + size() + ", requested "
+            + size);
+      allocateMemory(size);
+    }
+
+    /**
+     * WARNING: this filename may be either the short or long form
+     * of the filename.
+     */
+    public String getFilename()
+    {
+      return new String(FileName, 0, FileNameLength / 2);
+    }
+
+    public void read()
+    {
+      // avoid reading filename until we know how long it is
+      FileName = new char[0];
+      super.read();
+      FileName = getPointer().getCharArray(12, FileNameLength / 2);
+    }
+
+    public FILE_NOTIFY_INFORMATION next()
+    {
+      if(NextEntryOffset == 0)
+        return null;
+      FILE_NOTIFY_INFORMATION next = new FILE_NOTIFY_INFORMATION();
+      next.useMemory(getPointer(), NextEntryOffset);
+      next.read();
+      return next;
+    }
+  }
+
+  class OVERLAPPED extends Structure
+  {
+    public int Internal;
+    public int InternalHigh;
+    public int Offset;
+    public int OffsetHigh;
+    public Pointer hEvent;
+  }
+
+  // TODO: figure out how OVERLAPPED is used and apply an appropriate mapping
+  interface OVERLAPPED_COMPLETION_ROUTINE extends StdCallCallback
+  {
+    void callback(int errorCode, int nBytesTransferred, OVERLAPPED overlapped);
+  }
+
+  /**
+   * NOTE: only exists in unicode form (W suffix).  Define this method
+   * explicitly with the W suffix to avoid inadvertent calls in ASCII mode.
+   */
+  boolean ReadDirectoryChangesW(HANDLE directory,
+                                FILE_NOTIFY_INFORMATION info,
+                                int length,
+                                boolean watchSubtree,
+                                int notifyFilter,
+                                IntByReference bytesReturned,
+                                OVERLAPPED overlapped,
+                                OVERLAPPED_COMPLETION_ROUTINE completionRoutine);
+
+  /**
+   * ASCII version.  Use {@link Native#toString(byte[])} to obtain the short
+   * path from the <code>byte</code> array.
+   * Use only if <code>w32.ascii==true</code>.
+   */
+  int GetShortPathName(String lpszLongPath, byte[] lpdzShortPath, int cchBuffer);
+
+  /**
+   * Unicode version (the default).  Use {@link Native#toString(char[])} to
+   * obtain the short path from the <code>char</code> array.
+   */
+  int GetShortPathName(String lpszLongPath, char[] lpdzShortPath, int cchBuffer);
+}
+
---
+++ src/limelight/os/win32/Win32OS.java
@@ -0,0 +1,78 @@
+package limelight.os.win32;
+
+import limelight.os.OS;
+
+public class Win32OS extends OS
+{
+  private int hookThreadId;
+
+  protected void turnOnKioskMode()
+  {
+    new Thread()
+    {
+      public void run()
+      {
+        W32API.HINSTANCE appInstance = Kernel32.INSTANCE.GetModuleHandle(null);
+        final User32.HHOOK keystrokeHook = User32.INSTANCE.SetWindowsHookEx(User32.WH_KEYBOARD_LL, new KeyboardHandler(), appInstance, 0);
+
+        hookThreadId = Kernel32.INSTANCE.GetCurrentThreadId();
+        MsgLoop();
+        User32.INSTANCE.UnhookWindowsHookEx(keystrokeHook);
+      }
+    }.start();
+  }
+
+  protected void turnOffKioskMode()
+  {
+    if(hookThreadId == 0)
+      return;
+
+    User32.INSTANCE.PostThreadMessage(new W32API.DWORD(hookThreadId), User32.WM_QUIT, new W32API.WPARAM(0), new W32API.LPARAM(0));
+  }
+
+  private void MsgLoop()
+  {
+    User32.MSG message = new User32.MSG();
+
+    while(User32.INSTANCE.GetMessage(message, null, 0, 0) != 0)
+    {
+      User32.INSTANCE.TranslateMessage(message);
+      User32.INSTANCE.DispatchMessage(message);
+    }
+  }
+
+  private static class KeyboardHandler implements User32.LowLevelKeyboardProc
+  {
+    public W32API.LRESULT callback(int nCode, W32API.WPARAM wParam, User32.KBDLLHOOKSTRUCT lParam)
+    {
+      boolean shouldEatKeys = false;
+
+      if(nCode == User32.HC_ACTION)
+      {
+        switch(wParam.intValue())
+        {
+          case User32.WM_KEYDOWN:
+          case User32.WM_SYSKEYDOWN:
+          case User32.WM_KEYUP:
+          case User32.WM_SYSKEYUP:
+
+            boolean alt = (lParam.flags & User32.LLKHF_ALTDOWN) != 0;
+            boolean ctrl = (User32.INSTANCE.GetKeyState(User32.VK_CONTROL) & 0x8000) != 0;
+            boolean tab = lParam.vkCode == User32.VK_TAB;
+            boolean esc = lParam.vkCode == User32.VK_ESCAPE;
+            boolean del = lParam.vkCode == User32.VK_DELETE;
+            boolean space = lParam.vkCode == User32.VK_SPACE;
+            boolean windowsKey = (lParam.vkCode == 91) || (lParam.vkCode == 92) || (lParam.vkCode == 93);
+
+            shouldEatKeys = (alt && tab) || (alt && esc) || (alt && space) || (ctrl && esc) || windowsKey || (ctrl && alt && del);
+
+//            System.err.println("alt:" + alt + " ctrl:" + ctrl + " tab:" + tab + " del:" + del + " windowsKey:" + windowsKey);
+
+            break;
+        }
+      }
+      int value = shouldEatKeys ? 1 : User32.INSTANCE.CallNextHookEx(new User32.HHOOK(), nCode, wParam, lParam.getPointer()).intValue();
+      return new W32API.LRESULT(value);
+    }
+  }
+}
