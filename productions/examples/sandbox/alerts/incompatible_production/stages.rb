24ea1e8f854141897cfc050ec00626c050f54be9
Micah <micah@8thlight.com>
2009-08-20 10:26:25 -0500

    updated copyrights

--- productions/examples/sandbox/alerts/incompatible_production/stages.rb
+++ productions/examples/sandbox/alerts/incompatible_production/stages.rb
@@ -1,3 +1,6 @@
+#- Copyright © 2008-2009 8th Light, Inc. All Rights Reserved.
+#- Limelight and all included source files are distributed under terms of the GNU LGPL.
+
 # This file (stages.rb) is used to define the stages within your production.
 #
 # Below is an example statge configuration.
--- productions/examples/sandbox/kiosk/props.rb
+++ productions/examples/sandbox/kiosk/props.rb
@@ -1,3 +1,6 @@
+#- Copyright © 2008-2009 8th Light, Inc. All Rights Reserved.
+#- Limelight and all included source files are distributed under terms of the GNU LGPL.
+
 __install "header.rb"
 arena do
   action :id => "kiosk_button", :text => "Toggle Kiosk Mode: OFF", :on_mouse_clicked => "scene.toggle_kiosk_mode"
