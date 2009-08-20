8980207cb568883e021498997db7399bc0744067
Micah <micah@8thlight.com>
2009-08-20 10:26:25 -0500

    updated copyrights

--- lib/limelight/builtin/utilities_production/alert/styles.rb
+++ lib/limelight/builtin/utilities_production/alert/styles.rb
@@ -1,3 +1,6 @@
+#- Copyright © 2008-2009 8th Light, Inc. All Rights Reserved.
+#- Limelight and all included source files are distributed under terms of the GNU LGPL.
+
 alert {
   background_color "#b2db5b"
   secondary_background_color "#8fc927"
