0f35c358850d1813f77035ca1bedddfb31a3cd57
Eric Smith <eric@8thlight.com>
2009-10-09 14:28:11 -0500

    Implemented uses_limelight.  Time to do some cleanup
    A little cleanup.

---
+++ lib/limelight/specs/test_scene_opener.rb
@@ -0,0 +1,60 @@
+module Limelight
+  module Specs
+    module SpecHelper
+
+      class TestSceneOpener
+        def initialize(producer, ll_spec_options, prop_block)
+          @producer = producer
+          @prop_block = prop_block
+          @production = producer.production
+          @ll_spec_options = ll_spec_options
+        end
+  
+        def create_player_helper
+          player_name = @ll_spec_options[:with_player]
+          Limelight.build_props(@scene, :build_loader => @production.root) do
+            __test_prop({:name =>player_name, :players => "#{player_name}", :id => "#{player_name}"})
+          end        
+        end
+
+        def setup_player_spec
+          path = @production.scene_directory(@ll_spec_options[:scene_path]) 
+          @scene = Scene.new(:production => @production, :casting_director => @producer.casting_director, :path => path, :name => @ll_spec_options[:scene_name])
+
+          if @prop_block
+            Limelight.build_props(@scene, :build_loader => @production.root, &@prop_block)
+          elsif @ll_spec_options[:with_player]
+            create_player_helper
+          end
+
+          styles = @producer.load_styles(@scene)
+          @scene.styles = @producer.load_styles(@scene)
+          @stage.open(@scene)
+        end
+
+        def setup_stage
+          if @ll_spec_options[:stage]
+            @stage = @producer.theater[@ll_spec_options[:stage]]
+            raise "No such stage: '#{@ll_spec_options[:stage]}'" unless stage
+          else
+            @stage = @producer.theater.default_stage
+          end
+
+          @stage.should_remain_hidden = @ll_spec_options[:hidden] || true
+        end
+
+        def open_scene
+          setup_stage
+
+          if @ll_spec_options[:scene]
+            @scene = @producer.open_scene(@ll_spec_options[:scene].to_s, @stage)
+          elsif @ll_spec_options[:scene_path]
+            setup_player_spec
+          end
+    
+          return @scene
+        end
+      end
+    end
+  end
+end
