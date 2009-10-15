10656d467c5d43bb9a1a2509cdda2e6f2f001355
Micah <micah@8thlight.com>
2009-10-15 09:06:26 -0500

    rdoc corrections and fix

--- lib/limelight/animation.rb
+++ lib/limelight/animation.rb
@@ -3,7 +3,7 @@
                     
 module Limelight
 
-  # An object to manage a seqeunce of changes, assuably to a prop.  The Animation object is constructed with options
+  # An object to manage a seqeunce of changes, assumedly to a prop.  The Animation object is constructed with options
   # and a block.  Once the Animation is started, the block is invoked repeatedly until the Animation is stopped.
   #
   # Although, this object does not update the screen, it provides a means to perform sequential updates in evenly
@@ -11,8 +11,9 @@ module Limelight
   #
   class Animation < Limelight::Background::Animation
 
-    # A Prop and block are required to construct an Animation.  Options may include :name (string), :updates_per_second
-    # (int defaults to 60)
+    # A Prop and block are required to construct an Animation.  Options may include
+    # 1. :name (string)
+    # 2. :updates_per_second (int defaults to 60)
     #
     #   animation = Animation.new(prop, Proc.new { "do something"}, :updates_per_second => 20)
     #   animation.start
