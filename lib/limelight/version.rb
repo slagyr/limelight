c1183fcf9884f1dfecb3b2d6da840718c9c53dd7
Micah <micah@8thlight.com>
2009-11-01 20:35:31 -0600

    upping version

--- lib/limelight/version.rb
+++ lib/limelight/version.rb
@@ -6,7 +6,7 @@ module Limelight
     unless defined? MAJOR
       MAJOR  = 0
       MINOR  = 5
-      TINY   = 3
+      TINY   = 4
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       TAG    = "REL_" + [MAJOR, MINOR, TINY].join('_')
