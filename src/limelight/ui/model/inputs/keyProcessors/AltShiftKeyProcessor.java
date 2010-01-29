dde270fe2ea47760dd431347b7411cf1eb52d330
LupusDei <lupusdei108@gmail.com>
2010-01-30 06:28:34 +0800

    Refactored a lot of names and the WordSelector

---
+++ src/limelight/ui/model/inputs/keyProcessors/AltShiftKeyProcessor.java
@@ -0,0 +1,32 @@
+package limelight.ui.model.inputs.keyProcessors;
+
+import limelight.ui.model.inputs.KeyProcessor;
+import limelight.ui.model.inputs.TextModel;
+
+import java.awt.event.KeyEvent;
+
+public class AltShiftKeyProcessor extends KeyProcessor
+{
+  public AltShiftKeyProcessor(TextModel boxInfo)
+  {
+    super(boxInfo);
+  }
+
+  public void processKey(KeyEvent event)
+  {
+    int keyCode = event.getKeyCode();
+
+    if (isACharacter(keyCode))
+    {
+      insertCharIntoTextBox(event.getKeyChar());
+    }
+    else if(isMoveRightEvent(keyCode)){
+      initSelection();
+      boxInfo.setCursorIndex(findNearestWordToTheRight());
+    }
+    else if(isMoveLeftEvent(keyCode)){
+      initSelection();
+      boxInfo.setCursorIndex(findNearestWordToTheLeft());
+    }
+  }
+}
---
+++ src/limelight/ui/model/inputs/keyProcessors/SelectionOnShiftCmdKeyProcessor.java
@@ -0,0 +1,26 @@
+package limelight.ui.model.inputs.keyProcessors;
+
+import limelight.ui.model.inputs.KeyProcessor;
+import limelight.ui.model.inputs.TextModel;
+
+import java.awt.event.KeyEvent;
+
+public class SelectionOnShiftCmdKeyProcessor extends KeyProcessor
+{
+  public SelectionOnShiftCmdKeyProcessor(TextModel boxInfo)
+  {
+    super(boxInfo);
+  }
+
+  public void processKey(KeyEvent event)
+  {
+    int keyCode = event.getKeyCode();
+
+    if(isMoveRightEvent(keyCode)) {
+      boxInfo.setCursorIndex(boxInfo.getText().length());
+    }
+    else if(isMoveLeftEvent(keyCode)) {
+      boxInfo.setCursorIndex(0);
+    }
+  }
+}
