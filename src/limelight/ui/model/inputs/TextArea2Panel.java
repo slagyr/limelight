88641c4a7dfebf90bde1e8953fa6baa0d75c8b30
Justin Martin <lupusdei108@gmail.com>
2010-02-02 12:27:01 -0600

    added text area, and added textbox tests

---
+++ src/limelight/ui/model/inputs/TextArea2Panel.java
@@ -0,0 +1,44 @@
+package limelight.ui.model.inputs;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.Transferable;
+
+public class TextArea2Panel   extends TextInputPanel
+
+{
+  @Override
+  public void initKeyProcessors()
+  {
+  }
+
+  @Override
+  public void paintOn(Graphics2D graphics)
+  {
+  }
+
+  @Override
+  public void setPaintableRegion(int index)
+  {
+  }
+
+  @Override
+  public void resetPaintableRegion()
+  {
+  }
+
+  @Override
+  public void maxOutPaintableRegion()
+  {
+  }
+
+  @Override
+  public boolean isTextMaxed()
+  {
+    return false;
+  }
+
+  public void lostOwnership(Clipboard clipboard, Transferable contents)
+  {
+  }
+}
