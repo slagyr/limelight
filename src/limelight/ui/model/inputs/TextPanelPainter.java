3df4cdc66ea79313701f588df74192e56eb56992
Micah <micah@8thlight.com>
2010-03-20 14:14:23 -0500

    added sandbox for text_area

--- src/limelight/ui/model/inputs/TextPanelPainter.java
+++ src/limelight/ui/model/inputs/TextPanelPainter.java
@@ -1,12 +1,11 @@
 package limelight.ui.model.inputs;
 
-import limelight.ui.MockGraphics;
-
 import java.awt.*;
 
 public abstract class TextPanelPainter
 {
-  public static final int HEIGHT_MARGIN = 4;
+  //TODO Is this the same thing as TextModel.TOP_MARGIN 
+  public static final int HEIGHT_MARGIN = 7;
   protected TextModel boxInfo;
   public boolean hasPainted;
   public TextPanelPainter(TextModel boxInfo)
