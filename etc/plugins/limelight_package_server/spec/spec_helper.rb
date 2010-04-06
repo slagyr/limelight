f22c53141b6b2d2dd728560bf21d5f7d8d44b5d5
Micah <micah@8thlight.com>
2010-04-06 01:13:10 -0500

    a bit of spring cleaning

--- etc/plugins/limelight_package_server/spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-#- Copyright © 2008-2010 8th Light, Inc. All Rights Reserved.
-#- Limelight and all included source files are distributed under terms of the GNU LGPL.
-
-require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
-
-plugin_spec_dir = File.dirname(__FILE__)
-ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/debug.log")
-
