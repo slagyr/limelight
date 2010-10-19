30f1ffb512eab75d1c7b56b86c3326a826298861
Micah <micah@8thlight.com>
2010-10-19 17:26:15 -0500

    comitting most recent changes

--- clj/src/limelight/stage.clj
+++ clj/src/limelight/stage.clj
@@ -1,6 +1,12 @@
 (ns limelight.stage)
 
-(deftype Stage []
+(deftype Stage [peer theater]
   limelight.model.api.StageProxy
   (applyOptions [this options] nil))
+
+(defn new-stage [theater name options]
+  (let [stage (Stage. (atom nil) theater) peer (limelight.ui.model.FramedStage. name stage)]
+    (swap! (.peer stage) (fn [_] peer))
+    ; apply-option
+    stage))
 
--- clj/test/limelight/test/stage.clj
+++ clj/test/limelight/test/stage.clj
@@ -1,10 +1,17 @@
 (ns limelight.test.stage
   (:use
     [limelight.stage]
-    [lazytest.describe :only (describe testing it with)]
+    [lazytest.describe :only (describe testing it with given)]
     [lazytest.context :only (fn-context)]))
 
-(describe "Stage lineage"
-  (it (some #{limelight.model.api.StageProxy} (supers limelight.stage.Stage))))
+(describe "Stage"
+  (testing "lineage"
+    (it "implements the API" (some #{limelight.model.api.StageProxy} (supers limelight.stage.Stage))))
+  (testing "creation"
+    (given [stage (new-stage :theater "Bill" (hash-map :options nil))]
+      (it "makes a peer" (= limelight.ui.model.FramedStage (type @(.peer stage))))
+      (it "peer has stage" (= stage (.getProxy @(.peer stage))))
+      (it "peer is named" (= "Bill" (.getName @(.peer stage))))
+      (it "has theater" (= :theater (.theater stage))))))
 
 
