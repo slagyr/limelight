06a8f21bf25d44e11ccd3ac6c786048b0f9d5c69
Colin Jones <trptcolin@gmail.com>
2010-10-19 20:37:21 -0500

    Implement buildStage on Theater.

--- clj/src/limelight/theater.clj
+++ clj/src/limelight/theater.clj
@@ -1,5 +1,7 @@
-(ns limelight.theater)
+(ns limelight.theater
+  (:use [limelight.stage :only (new-stage)]))
 
 (deftype Theater [peer production]
   limelight.model.api.TheaterProxy
-  (buildStage [this name options] nil))
+  (buildStage [this name options]
+    @(.peer (new-stage this name options))))
