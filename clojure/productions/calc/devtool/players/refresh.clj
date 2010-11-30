5e90df82124811ac6ab58f1b070c0733038d0e96
Micah <micah@8thlight.com>
2010-11-30 14:38:05 -0600

    either ruby or clojure productions can be loaded

--- clojure/productions/calc/devtool/players/refresh.clj
+++ clojure/productions/calc/devtool/players/refresh.clj
@@ -1,19 +1,12 @@
-(on-mouse-clicked
-  (fn [e]
-    (let [scene (.getRoot (.getRecipient e))
-          stage (.getStage scene)
-          production (.getProduction scene)
-          stages (.getStages (.getTheater production))]
-      (println stages)
-      (doall (map
-        (fn [stage]
-          (println "stage: " stage)
-          (.openScene production (.getName (.getScene stage)) stage (limelight.util.OptionsMap. {})))
-        stages))
-      )))
-;  production.theater.stages.each do |stage|
-;    if stage.current_scene
-;      production.producer.open_scene(stage.current_scene.name, stage)
-;    end
-;  end
-;end
+(on-mouse-clicked [e]
+  (let [scene (.getRoot (.getRecipient e))
+        stage (.getStage scene)
+        production (.getProduction scene)
+        stages (.getStages (.getTheater production))]
+    (println stages)
+    (doall (map
+      (fn [stage]
+        (println "stage: " stage)
+        (.openScene production (.getName (.getScene stage)) stage (limelight.util.OptionsMap. {})))
+      stages))
+    ))
