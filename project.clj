ca31751171c830a10a570f45c7b58f8e98e70017
Micah <micah@8thlight.com>
2011-08-17 07:45:10 -0500

    added project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject limelight "0.1.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [jruby-complete "1.6.3"]
+                 [jna "3.0.9"]]
+  :dev-dependencies [[junit "4.8.2"]]
+  )
