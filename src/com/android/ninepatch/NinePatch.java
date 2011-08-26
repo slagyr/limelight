661f004baedbca3af83525548826db7cd9c5c47f
Micah <micah@8thlight.com>
2011-08-26 17:30:05 -0500

    updated copyright header for all java src

--- src/com/android/ninepatch/NinePatch.java
+++ src/com/android/ninepatch/NinePatch.java
@@ -1,5 +1,5 @@
-//- Copyright © 2008-2010 8th Light, Inc. All Rights Reserved.
-//- Limelight and all included source files are distributed under terms of the GNU LGPL.
+//- Copyright © 2008-2011 8th Light, Inc. All Rights Reserved.
+//- Limelight and all included source files are distributed under terms of the MIT License.
 
 /*
  * Copyright (C) 2008 The Android Open Source Project
