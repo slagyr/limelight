81567d294d3a0258ccaaf6049d269965c5b70879
Micah <micah@8thlight.com>
2011-08-26 17:36:53 -0500

    updated clojure copyright headers

--- clojure/src/limelight/scene.clj
+++ clojure/src/limelight/scene.clj
@@ -1,3 +1,6 @@
+;- Copyright © 2008-2011 8th Light, Inc. All Rights Reserved.
+;- Limelight and all included source files are distributed under terms of the MIT License.
+
 (ns limelight.scene
   (:use
     [limelight.casting :only (new-casting-director)]
