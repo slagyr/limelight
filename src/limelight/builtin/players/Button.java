46e179b7c0a66eb51b1e1681c51313c5d09518eb
Micah <micah@8thlight.com>
2011-08-26 17:30:05 -0500

    updated copyright header for all java src

--- src/limelight/builtin/players/Button.java
+++ src/limelight/builtin/players/Button.java
@@ -1,3 +1,6 @@
+//- Copyright © 2008-2011 8th Light, Inc. All Rights Reserved.
+//- Limelight and all included source files are distributed under terms of the MIT License.
+
 package limelight.builtin.players;
 
 import limelight.ui.events.panel.PanelEvent;
--- src/limelight/styles/Styles.java
+++ src/limelight/styles/Styles.java
@@ -1,3 +1,6 @@
+//- Copyright © 2008-2011 8th Light, Inc. All Rights Reserved.
+//- Limelight and all included source files are distributed under terms of the MIT License.
+
 package limelight.styles;
 
 import java.util.HashMap;
