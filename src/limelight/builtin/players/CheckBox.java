4e3a9f92206a7e9faddf87cbec4cff58cdf48da3
Micah <micah@8thlight.com>
2011-12-15 14:21:14 -0600

    added useful methods on CheckBox player

--- src/limelight/builtin/players/CheckBox.java
+++ src/limelight/builtin/players/CheckBox.java
@@ -6,10 +6,43 @@ import limelight.ui.model.inputs.CheckBoxPanel;
 
 public class CheckBox
 {
+  private PropPanel propPanel;
+  private CheckBoxPanel checkBoxPanel;
+
   public void install(PanelEvent event)
   {
-    CheckBoxPanel input = new CheckBoxPanel();
-    final PropPanel panel = (PropPanel)event.getRecipient();
-    panel.add(input);
+    checkBoxPanel = new CheckBoxPanel();
+    propPanel = (PropPanel)event.getRecipient();
+    propPanel.add(checkBoxPanel);
+  }
+
+  public PropPanel getPropPanel()
+  {
+    return propPanel;
+  }
+
+  public CheckBoxPanel getCheckBoxPanel()
+  {
+    return checkBoxPanel;
+  }
+
+  public boolean isChecked()
+  {
+    return checkBoxPanel.isSelected();
+  }
+
+  public boolean isSelected()
+  {
+    return checkBoxPanel.isSelected();
+  }
+
+  public void setChecked(boolean b)
+  {
+    checkBoxPanel.setSelected(b);
+  }
+
+  public void setSelected(boolean b)
+  {
+    checkBoxPanel.setSelected(b);
   }
 }
---
+++ test/limelight/builtin/players/CheckBoxTest.java
@@ -0,0 +1,56 @@
+package limelight.builtin.players;
+
+import limelight.model.api.FakePropProxy;
+import limelight.ui.events.panel.CastEvent;
+import limelight.ui.model.PropPanel;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class CheckBoxTest
+{
+
+  public CheckBox checkBox;
+  public PropPanel propPanel;
+
+  @Before
+  public void setUp() throws Exception
+  {
+    checkBox = new CheckBox();
+    propPanel = new PropPanel(new FakePropProxy());
+    checkBox.install(new CastEvent(propPanel));
+  }
+
+  @Test
+  public void installation() throws Exception
+  {
+    assertEquals(propPanel, checkBox.getPropPanel());
+    assertNotNull(checkBox.getCheckBoxPanel());
+    assertEquals(checkBox.getCheckBoxPanel(), propPanel.getChildren().get(0));
+    assertEquals(true, propPanel.isSterilized());
+  }
+
+  @Test
+  public void isChecked() throws Exception
+  {
+    assertEquals(false, checkBox.isChecked());
+    assertEquals(false, checkBox.isSelected());
+
+    checkBox.getCheckBoxPanel().setSelected(true);
+
+    assertEquals(true, checkBox.isChecked());
+    assertEquals(true, checkBox.isSelected());
+  }
+
+  @Test
+  public void checking() throws Exception
+  {
+    checkBox.setChecked(true);
+    assertEquals(true, checkBox.getCheckBoxPanel().isSelected());
+
+    checkBox.setSelected(false);
+    assertEquals(false, checkBox.getCheckBoxPanel().isSelected());
+  }
+}
