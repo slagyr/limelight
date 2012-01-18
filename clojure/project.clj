eaf5b2683ec1b3d1a0ada3fad55dca928e559a14
Micah <micah@8thlight.com>
2012-01-18 16:43:38 -0600

    incrementing clj version

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject limelight/limelight-clj "0.6.11"
+(defproject limelight/limelight-clj "0.6.12"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[speclj "2.0.1"]
